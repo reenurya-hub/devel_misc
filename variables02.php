9ec907d6541afb936b553fd84d5db13432094863
Reinaldo Urquijo <reinaldo.urquijo@gmail.com>
2023-02-14 21:19:29 -0500

    Add files via upload

---
+++ variables02.php
@@ -0,0 +1,13 @@
+<?php
+$var1 = 10;
+$var2 = 20;
+$var3 = 30;
+// Sumar los 3 valores y mostrar el resultado
+$result = $var1 + $var2 + $var3;
+echo "\n".$result;
+// mostrar el resultado de la resta del tercero menos el primero
+$result = $var3 - $var1;
+echo "\n".$result;
+// mostrar el residuo de la division del tercero con el segundo
+$result = $var3 % $var2;
+echo "\n".$result;
