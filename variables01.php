db5bd8c0d02595c047724867f3e3f0fe205419be
Reinaldo Urquijo <reinaldo.urquijo@gmail.com>
2023-02-14 21:07:09 -0500

    Add files via upload

---
+++ variables01.php
@@ -0,0 +1,15 @@
+<?php
+//$variable1 = 130;  // es entero
+//echo $variable1;
+// $edadEsposaProfe = 38;
+// $nombreProfesor = 'luigi, tiene $edadEsposaProfe';
+// $nombreEsposaProfe = "Angela tiene $edadEsposaProfe";
+//$miNombre = 'Reinaldo';
+//$miEdad = 38;
+//$miEdad2 = 1;
+//echo "Hola, soy $miNombre, tengo $miEdad años\n";
+//echo 'Hola, soy $miNombre, tengo $miEdad años\n';
+//echo "\nHola, soy " . $miNombre . ', tengo ' . $miEdad . " años\n";
+$soyPeludo = false;
+var_dump($soyPeludo);
+//echo "\nHola, soy " . $miNombre . ', tengo ' . $miEdad . " años y soy ".$soyPeludo."\n";
