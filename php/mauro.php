d080578d84f3f7167462ece6a702900a6dac5c9a
Reinaldo Urquijo <reinaldo.urquijo@gmail.com>
2023-02-16 19:06:11 -0500

    Add files via upload

---
+++ php/mauro.php
@@ -0,0 +1,10 @@
+<?php 
+$firstName   = "Luis";
+$secondName  = "Osorio";
+$personAge   = 44;
+$alumnosSeminario = array("Henry", "Carlos", "Luisa", "Reinaldo");
+echo "hola, yo soy $firstName $secondName y estoy en el Diplomado. Tengo $personAge años";
+echo "\n";
+var_dump($alumnosSeminario);
+
+?>
