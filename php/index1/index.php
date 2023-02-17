3b88e62bb7e06ee3c4a03000de77d97a2a35d4b8
Reinaldo Urquijo <reinaldo.urquijo@gmail.com>
2023-02-16 19:31:11 -0500

    Add files via upload

---
+++ php/index1/index.php
@@ -0,0 +1,10 @@
+<?php
+//echo phpinfo();
+//die;
+echo "Hola mundo!\n";
+//echo "PHP version:".phpversion();
+//echo $_SERVER["HTTP_USER_AGENT"];
+#echo $_SERVER["HTTP_ACCEPT_LANGUAGE"];
+//var_dump($_SERVER);
+
+?>
