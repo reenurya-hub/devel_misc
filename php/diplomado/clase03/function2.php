235dfee7807b2b570731280ce626e3559536820c
Reinaldo Urquijo <reinaldo.urquijo@gmail.com>
2023-02-20 11:34:07 -0500

    Create function2.php

---
+++ php/diplomado/clase03/function2.php
@@ -0,0 +1,28 @@
+<?php
+
+$hacer_algo = true;
+
+/* No podemos llamar a foo() desde aquí
+   ya que no existe aún,
+   pero podemos llamar a bar() */
+
+bar();
+
+if ($hacer_algo) {
+  function foo()
+  {
+    echo "No existo hasta que la ejecución del programa llegue hasta mí.\n";
+  }
+}
+
+/* Ahora podemos llamar de forma segura a foo()
+   ya que $hacer_algo se evaluó como verdadero */
+
+if ($hacer_algo) foo();
+
+function bar()
+{
+  echo "Existo desde el momento inmediato que comenzó el programa.\n";
+}
+
+?>
