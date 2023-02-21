8b538d7af91106559fd82534cbdb5ab24b1ad8ef
Reinaldo Urquijo <reinaldo.urquijo@gmail.com>
2023-02-21 08:18:31 -0500

    Create libreria.php

---
+++ php/diplomado/clase03/includeCodHTML/libreria.php
@@ -0,0 +1,8 @@
+<?
+//función de encabezado y colocación del titulo
+function hacer_encabezado($titulo)
+{
+  $encabezado="<html>n<head>nt<title>$titulo</title>n</head>n";
+  echo $encabezado;
+}
+?>
