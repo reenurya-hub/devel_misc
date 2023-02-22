4d78cf6c3df9860bbcdf30b96b3eeda4d0d413a2
Reinaldo Urquijo <reinaldo.urquijo@gmail.com>
2023-02-21 19:11:01 -0500

    Create figuresText.cpp

---
+++ algorithms/figures/figuresText.cpp
@@ -0,0 +1,6 @@
+/*
+...-... 7-1 6/2 = 3 imprime 3 espacios, un asterisco, 3 espacios
+..---..  7-3 4/2 = 2 imprime 2 espacios, tres asteriscos, 2 espacios
+.-----. 7-5 2/2 = 1  imprime 1 espacio, cinco asteriscos, 1 espacio
+------- 7-7 = 0/2 = 0 imprime 7 asteriscos
+*/
