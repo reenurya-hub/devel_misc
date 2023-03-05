a6a955fdf8dfc8ac741094f1dc52b3d837c23164
Reinaldo Urquijo <reinaldo.urquijo@gmail.com>
2023-03-04 22:38:40 -0500

    Update and rename AppleAndOrange.cpp to AppleAndOrange.java

--- hackerrank/AppleAndOrange.java
+++ hackerrank/AppleAndOrange.java
@@ -87,125 +87,82 @@ The second orange falls at position .
 Only one fruit (the second apple) falls within the region between and , so we print as our first line of output.
 Only the second orange falls within the region between and , so we print as our second line of output. 
 */
-#include <bits/stdc++.h>
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
 
-using namespace std;
+public class Solution {
 
-string ltrim(const string &);
-string rtrim(const string &);
-vector<string> split(const string &);
+    // Complete the countApplesAndOranges function below.
+    static void countApplesAndOranges(int s, int t, int a, int b, int[] apples, int[] oranges) {
+    int totap=0;
+    int totor=0;
+        for(int i=0; i<apples.length; i++){
+          apples[i]= apples[i] + a;
+          if(apples[i]>=s && apples[i] <=t)
+          {
+          totap++;
+          }
+        }
+      for(int i=0; i<oranges.length; i++){
+          oranges[i]= oranges[i] + b;
+          if(oranges[i]>=s && oranges[i] <=t)
+          {
+          totor++;
+          }
+      }
+System.out.println(totap);
+System.out.println(totor);
 
-/*
- * Complete the 'countApplesAndOranges' function below.
- *
- * The function accepts following parameters:
- *  1. INTEGER s
- *  2. INTEGER t
- *  3. INTEGER a
- *  4. INTEGER b
- *  5. INTEGER_ARRAY apples
- *  6. INTEGER_ARRAY oranges
- */
-
-void countApplesAndOranges(int s, int t, int a, int b, vector<int> apples, vector<int> oranges) {
-
-}
-
-int main()
-{
-    string first_multiple_input_temp;
-    getline(cin, first_multiple_input_temp);
-
-    vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
-
-    int s = stoi(first_multiple_input[0]);
-
-    int t = stoi(first_multiple_input[1]);
-
-    string second_multiple_input_temp;
-    getline(cin, second_multiple_input_temp);
-
-    vector<string> second_multiple_input = split(rtrim(second_multiple_input_temp));
-
-    int a = stoi(second_multiple_input[0]);
-
-    int b = stoi(second_multiple_input[1]);
-
-    string third_multiple_input_temp;
-    getline(cin, third_multiple_input_temp);
-
-    vector<string> third_multiple_input = split(rtrim(third_multiple_input_temp));
-
-    int m = stoi(third_multiple_input[0]);
-
-    int n = stoi(third_multiple_input[1]);
-
-    string apples_temp_temp;
-    getline(cin, apples_temp_temp);
-
-    vector<string> apples_temp = split(rtrim(apples_temp_temp));
-
-    vector<int> apples(m);
-
-    for (int i = 0; i < m; i++) {
-        int apples_item = stoi(apples_temp[i]);
-
-        apples[i] = apples_item;
     }
 
-    string oranges_temp_temp;
-    getline(cin, oranges_temp_temp);
+    private static final Scanner scanner = new Scanner(System.in);
 
-    vector<string> oranges_temp = split(rtrim(oranges_temp_temp));
+    public static void main(String[] args) {
+        String[] st = scanner.nextLine().split(" ");
 
-    vector<int> oranges(n);
+        int s = Integer.parseInt(st[0]);
 
-    for (int i = 0; i < n; i++) {
-        int oranges_item = stoi(oranges_temp[i]);
+        int t = Integer.parseInt(st[1]);
 
-        oranges[i] = oranges_item;
+        String[] ab = scanner.nextLine().split(" ");
+
+        int a = Integer.parseInt(ab[0]);
+
+        int b = Integer.parseInt(ab[1]);
+
+        String[] mn = scanner.nextLine().split(" ");
+
+        int m = Integer.parseInt(mn[0]);
+
+        int n = Integer.parseInt(mn[1]);
+
+        int[] apples = new int[m];
+
+        String[] applesItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < m; i++) {
+            int applesItem = Integer.parseInt(applesItems[i]);
+            apples[i] = applesItem;
+        }
+
+        int[] oranges = new int[n];
+
+        String[] orangesItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int orangesItem = Integer.parseInt(orangesItems[i]);
+            oranges[i] = orangesItem;
+        }
+
+        countApplesAndOranges(s, t, a, b, apples, oranges);
+
+        scanner.close();
     }
-
-    countApplesAndOranges(s, t, a, b, apples, oranges);
-
-    return 0;
-}
-
-string ltrim(const string &str) {
-    string s(str);
-
-    s.erase(
-        s.begin(),
-        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
-    );
-
-    return s;
-}
-
-string rtrim(const string &str) {
-    string s(str);
-
-    s.erase(
-        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
-        s.end()
-    );
-
-    return s;
-}
-
-vector<string> split(const string &str) {
-    vector<string> tokens;
-
-    string::size_type start = 0;
-    string::size_type end = 0;
-
-    while ((end = str.find(" ", start)) != string::npos) {
-        tokens.push_back(str.substr(start, end - start));
-
-        start = end + 1;
-    }
-
-    tokens.push_back(str.substr(start));
-
-    return tokens;
 }
