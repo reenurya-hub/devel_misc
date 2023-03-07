ab9124ce3e5a9bca8cac7e034e19f712636c697d
Reinaldo Urquijo <reinaldo.urquijo@gmail.com>
2023-03-07 08:48:04 -0500

    Update comparethetriplets.cpp

--- hackerrank/comparethetriplets.cpp
+++ hackerrank/comparethetriplets.cpp
@@ -1,106 +1,150 @@
-#include <bits/stdc++.h>
-
-using namespace std;
-
-string ltrim(const string &);
-string rtrim(const string &);
-vector<string> split(const string &);
-
 /*
- * Complete the 'compareTriplets' function below.
- *
- * The function is expected to return an INTEGER_ARRAY.
- * The function accepts following parameters:
- *  1. INTEGER_ARRAY a
- *  2. INTEGER_ARRAY b
- */
+Alice and Bob each created one problem for HackerRank. A reviewer rates the two challenges, awarding points on a scale from 1 to 100 for three categories: problem clarity, originality, and difficulty.
 
-vector<int> compareTriplets(vector<int> a, vector<int> b) {
+The rating for Alice's challenge is the triplet a = (a[0], a[1], a[2]), and the rating for Bob's challenge is the triplet b = (b[0], b[1], b[2]).
 
-}
+The task is to find their comparison points by comparing a[0] with b[0], a[1] with b[1], and a[2] with b[2].
 
-int main()
-{
-    ofstream fout(getenv("OUTPUT_PATH"));
+    If a[i] > b[i], then Alice is awarded 1 point.
+    If a[i] < b[i], then Bob is awarded 1 point.
+    If a[i] = b[i], then neither person receives a point.
 
-    string a_temp_temp;
-    getline(cin, a_temp_temp);
+Comparison points is the total points a person earned.
 
-    vector<string> a_temp = split(rtrim(a_temp_temp));
+Given a and b, determine their respective comparison points.
 
-    vector<int> a(3);
+Example
 
-    for (int i = 0; i < 3; i++) {
-        int a_item = stoi(a_temp[i]);
+a = [1, 2, 3]
+b = [3, 2, 1]
 
-        a[i] = a_item;
-    }
+    For elements *0*, Bob is awarded a point because a[0] .
+    For the equal elements a[1] and b[1], no points are earned.
+    Finally, for elements 2, a[2] > b[2] so Alice receives a point.
 
-    string b_temp_temp;
-    getline(cin, b_temp_temp);
+The return array is [1, 1] with Alice's score first and Bob's second.
 
-    vector<string> b_temp = split(rtrim(b_temp_temp));
+Function Description
 
-    vector<int> b(3);
+Complete the function compareTriplets in the editor below.
 
-    for (int i = 0; i < 3; i++) {
-        int b_item = stoi(b_temp[i]);
+compareTriplets has the following parameter(s):
 
-        b[i] = b_item;
-    }
+    int a[3]: Alice's challenge rating
+    int b[3]: Bob's challenge rating
 
-    vector<int> result = compareTriplets(a, b);
+Return
 
-    for (size_t i = 0; i < result.size(); i++) {
-        fout << result[i];
+    int[2]: Alice's score is in the first position, and Bob's score is in the second.
 
-        if (i != result.size() - 1) {
-            fout << " ";
+Input Format
+
+The first line contains 3 space-separated integers, a[0], a[1], and a[2], the respective values in triplet a.
+The second line contains 3 space-separated integers, b[0], b[1], and b[2], the respective values in triplet b.
+
+Constraints
+
+    1 ≤ a[i] ≤ 100
+    1 ≤ b[i] ≤ 100
+
+Sample Input 0
+
+5 6 7
+3 6 10
+
+Sample Output 0
+
+1 1
+
+Explanation 0
+
+In this example:
+
+Now, let's compare each individual score:
+
+, so Alice receives
+point.
+, so nobody receives a point.
+, so Bob receives
+
+    point.
+
+Alice's comparison score is
+, and Bob's comparison score is . Thus, we return the array
+
+.
+
+Sample Input 1
+
+17 28 30
+99 16 8
+
+Sample Output 1
+
+2 1
+
+Explanation 1
+
+Comparing the
+elements, so Bob receives a point.
+Comparing the and elements, and so Alice receives two points.
+The return array is . 
+*/
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+public class Solution {
+
+    // Complete the compareTriplets function below.
+    static List<Integer> compareTriplets(List<Integer> a, List<Integer> b) {
+        int[] arr={0,0};
+        List<Integer> list = new ArrayList<>(arr.length);
+        for(int i=0; i<=2; i++){
+            if(a.get(i) < b.get(i)){
+                arr[0]++;
+            }
+            if(b.get(i) < a.get(i))
+            {
+                arr[1]++;
+            }
+
         }
+        list.add(arr[1]);
+        list.add(arr[0]);
+        return list;
     }
 
-    fout << "\n";
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
-    fout.close();
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
 
-    return 0;
-}
+        List<Integer> b = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
 
-string ltrim(const string &str) {
-    string s(str);
+        List<Integer> result = compareTriplets(a, b);
 
-    s.erase(
-        s.begin(),
-        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
-    );
+        bufferedWriter.write(
+            result.stream()
+                .map(Object::toString)
+                .collect(joining(" "))
+            + "\n"
+        );
 
-    return s;
-}
-
-string rtrim(const string &str) {
-    string s(str);
-
-    s.erase(
-        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
-        s.end()
-    );
-
-    return s;
-}
-
-vector<string> split(const string &str) {
-    vector<string> tokens;
-
-    string::size_type start = 0;
-    string::size_type end = 0;
-
-    while ((end = str.find(" ", start)) != string::npos) {
-        tokens.push_back(str.substr(start, end - start));
-
-        start = end + 1;
+        bufferedReader.close();
+        bufferedWriter.close();
     }
-
-    tokens.push_back(str.substr(start));
-
-    return tokens;
 }
