7471b01c41224488481382499d0c8a2755d08f0b
Reinaldo Urquijo <reinaldo.urquijo@gmail.com>
2023-03-07 10:45:11 -0500

    Create beetweentwosets.java

---
+++ hackerrank/beetweentwosets.java
@@ -0,0 +1,138 @@
+/*
+There will be two arrays of integers. Determine all integers that satisfy the following two conditions:
+
+    The elements of the first array are all factors of the integer being considered
+    The integer being considered is a factor of all elements of the second array
+
+These numbers are referred to as being between the two arrays. Determine how many such numbers exist.
+
+Example
+
+There are two numbers between the arrays: and .
+, , and for the first value.
+, and , for the second value. Return
+
+.
+
+Function Description
+
+Complete the getTotalX function in the editor below. It should return the number of integers that are betwen the sets.
+
+getTotalX has the following parameter(s):
+
+    int a[n]: an array of integers
+    int b[m]: an array of integers
+
+Returns
+
+    int: the number of integers that are between the sets
+
+Input Format
+
+The first line contains two space-separated integers,
+and , the number of elements in arrays and .
+The second line contains distinct space-separated integers where .
+The third line contains distinct space-separated integers where
+
+.
+
+Constraints
+
+Sample Input
+
+2 3
+2 4
+16 32 96
+
+Sample Output
+
+3
+
+Explanation
+
+2 and 4 divide evenly into 4, 8, 12 and 16.
+4, 8 and 16 divide evenly into 16, 32, 96.
+
+4, 8 and 16 are the only three numbers for which each element of a is a factor and each is a factor of all elements of b. 
+*/
+import java.io.*;
+import java.math.*;
+import java.text.*;
+import java.util.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    /*
+     * Complete the getTotalX function below.
+     */
+    static int getTotalX(int[] a, int[] b) {
+        /*
+         * Write your code here.
+         */
+int x=1,r=0,j=0,count=0;
+        int[] d = new int[101];
+        for(x=1;x<101;x++){
+            int c=0;
+                for(int i=0;i<a.length;i++) {
+                    if(x%a[i]==0&&x>=a[i]){
+                        c++;
+                    }
+                    }
+                if(c==a.length){
+                    d[j]=x;
+                    r++;
+                    j++;
+                }
+                }
+        for(j=0;j<r;j++){
+            int c=0;
+            for(int i=0;i<b.length;i++){
+                if(b[i]%d[j]==0){
+                    c++;
+                }
+            }
+            if(c==b.length){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private static final Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nm = scan.nextLine().split(" ");
+
+        int n = Integer.parseInt(nm[0].trim());
+
+        int m = Integer.parseInt(nm[1].trim());
+
+        int[] a = new int[n];
+
+        String[] aItems = scan.nextLine().split(" ");
+
+        for (int aItr = 0; aItr < n; aItr++) {
+            int aItem = Integer.parseInt(aItems[aItr].trim());
+            a[aItr] = aItem;
+        }
+
+        int[] b = new int[m];
+
+        String[] bItems = scan.nextLine().split(" ");
+
+        for (int bItr = 0; bItr < m; bItr++) {
+            int bItem = Integer.parseInt(bItems[bItr].trim());
+            b[bItr] = bItem;
+        }
+
+        int total = getTotalX(a, b);
+
+        bw.write(String.valueOf(total));
+        bw.newLine();
+
+        bw.close();
+    }
+}
