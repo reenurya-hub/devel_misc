760a7dc14316af71131fd078adfba55b4b9e491e
Reinaldo Urquijo <reinaldo.urquijo@gmail.com>
2023-03-07 08:40:13 -0500

    Update and rename Staircase.cpp to Staircase.java

--- hackerrank/Staircase.java
+++ hackerrank/Staircase.java
@@ -68,53 +68,41 @@ Explanation
 The staircase is right-aligned, composed of # symbols and spaces, and has a height and width of
 .
 */
-#include <bits/stdc++.h>
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
 
-using namespace std;
+public class Solution {
 
-string ltrim(const string &);
-string rtrim(const string &);
-
-/*
- * Complete the 'staircase' function below.
- *
- * The function accepts INTEGER n as parameter.
- */
-
-void staircase(int n) {
-
+    // Complete the staircase function below.
+    static void staircase(int n) {
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                if((i+j)>n){
+                    System.out.print("#");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+   System.out.println();
+        }
 }
 
-int main()
-{
-    string n_temp;
-    getline(cin, n_temp);
+    private static final Scanner scanner = new Scanner(System.in);
 
-    int n = stoi(ltrim(rtrim(n_temp)));
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
-    staircase(n);
+        staircase(n);
 
-    return 0;
-}
-
-string ltrim(const string &str) {
-    string s(str);
-
-    s.erase(
-        s.begin(),
-        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
-    );
-
-    return s;
-}
-
-string rtrim(const string &str) {
-    string s(str);
-
-    s.erase(
-        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
-        s.end()
-    );
-
-    return s;
+        scanner.close();
+    }
 }
